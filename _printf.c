dd3da7e74d24f9687cdb19e93268c6a375c8ee91
Danisile Johannah <77388022+Danisile@users.noreply.github.com>
2022-03-14 10:50:43 +0200

    Group work

--- _printf.c
+++ _printf.c
@@ -1,51 +1,45 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * _printf - formatted output conversion and print data.
- * @format: input string.
- *
- * Return: number of chars printed.
+ * _printf - prints any string with certain flags for modification
+ * @format: the string of characters to write to buffer
+ * Return: an integer that counts how many writes to the buffer were made
  */
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0, len = 0, ibuf = 0;
-	va_list arguments;
+	int i = 0, var = 0;
+	va_list v_ls;
+	buffer *buf;
 
-	int (*function)(va_list, char *, unsigned int);
-	char *buffer;
-	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
-	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
+	buf = buf_new();
+	if (buf == NULL)
 		return (-1);
-	if (!format[i])
-		return (0);
-	for (i = 0; format && format[i]; i++)
+	if (format == NULL)
+		return (-1);
+	va_start(v_ls, format);
+	while (format[i])
 	{
+		buf_wr(buf);
 		if (format[i] == '%')
 		{
-			if (format[i + 1] == '\0')
-			{	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-				return (-1);
+			var = opid(buf, v_ls, format, i);
+			if (var < 0)
+			{
+				i = var;
+				break;
 			}
-			else
-			{	function = get_print_func(format, i + 1);
-				if (function == NULL)
-				{
-					if (format[i + 1] == ' ' && !format[i + 2])
-						return (-1);
-					handl_buf(buffer, format[i], ibuf), len++, i--;
-				}
-				else
-				{
-					len += function(arguments, buffer, ibuf);
-					i += ev_print_func(format, i + 1);
-				}
-			} i++;
+			i += var;
+			continue;
 		}
-		else
-			handl_buf(buffer, format[i], ibuf), len++;
-		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
-			;
+		buf->str[buf->index] = format[i];
+		buf_inc(buf);
+		i++;
 	}
-	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-	return (len);
+	buf_write(buf);
+	if (var >= 0)
+		i = buf->overflow;
+	buf_end(buf);
+	va_end(v_ls);
+	return (i);
 }
