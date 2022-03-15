8c08485be0a28e6af9ba1a9469b912b7909b8677
Danisile Johannah <77388022+Danisile@users.noreply.github.com>
2022-03-15 10:53:08 +0200

    Group work

--- _printf.c
+++ _printf.c
@@ -1,45 +1,75 @@
 #include "main.h"
-#include <stdlib.h>
-
 /**
- * _printf - prints any string with certain flags for modification
- * @format: the string of characters to write to buffer
- * Return: an integer that counts how many writes to the buffer were made
+ * opFunction - Second function for _printf
+ * @cont: Contador
+ * @list: Lista arguments
+ * @tipos: Tipos que nos trae el JSON
+ * @format: Formato
+ * Return: Len
+*/
+int opFunction(int cont, va_list list, typedate tipos[], const char *format)
+{
+int i = 0, j = 0, band = 0, spaces = 0;
+while (format && format[i])
+{
+	band = 0;
+	if (format[i] == '%' && format[i + 1] == '\0')
+	return (-1);
+	else if (format[i] == '%' && format[i + 1] != '%')
+	{
+		for (j = 0; j < 7; j++)
+		{
+			if (format[i] == '%' && format[i + spaces + 1] == tipos[j].typec)
+			{
+				cont += tipos[j].fun(list);
+				i += spaces + 1;
+				band = 1;
+			}
+		}
+		if (band == 0 && format[i + 2] != '\n')
+		{
+			_putchar(format[i]);
+			cont++;
+
+		}
+		else if (band == 0 && format[i + 2] == '\n')
+		{
+			_putchar(format[i]);
+			cont++;
+		}
+	}
+	else if (format[i] == '%' && format[i + 1] == '%')
+	{
+		i += _putchar('%');
+		cont += 1;
+	}
+	else
+		cont += _putchar(format[i]);
+	i++;
+}
+	return (cont);
+}
+/**
+ * _printf - Printf!!
+ * @format: Format
+ * Return: len
  */
 int _printf(const char *format, ...)
 {
-	int i = 0, var = 0;
-	va_list v_ls;
-	buffer *buf;
+typedate tipos[] = {
+	{'s', printString}, {'c', printChar}, {'i', printInteger},
+	{'d', printDecimal}, {'b', printBinary}, {'o', printOctal},
+	{'u', printUnsigned}
+};
+va_list list;
+int cont = -1;
 
-	buf = buf_new();
-	if (buf == NULL)
-		return (-1);
-	if (format == NULL)
-		return (-1);
-	va_start(v_ls, format);
-	while (format[i])
-	{
-		buf_wr(buf);
-		if (format[i] == '%')
-		{
-			var = opid(buf, v_ls, format, i);
-			if (var < 0)
-			{
-				i = var;
-				break;
-			}
-			i += var;
-			continue;
-		}
-		buf->str[buf->index] = format[i];
-		buf_inc(buf);
-		i++;
-	}
-	buf_write(buf);
-	if (var >= 0)
-		i = buf->overflow;
-	buf_end(buf);
-	va_end(v_ls);
-	return (i);
+if (format != NULL)
+{
+va_start(list, format);
+cont = 0;
+cont = opFunction(cont, list, tipos, format);
+va_end(list);
+}
+return (cont);
 }
